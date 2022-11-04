e9b5866110dbba7beb2b9d71ef599e63d24da15c
MuratGuld <slimplus1952@gmail.com>
2022-11-04 22:44:31 +0100

    I updated my portfolio

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let navbar = document.querySelector(".navbar");
+let hamburgerLinks = document.getElementById("hamburger-menu");
 
 navbar.addEventListener("click", (e) => {
   if (e.target.tagName == "A") {
@@ -6,3 +7,19 @@ navbar.addEventListener("click", (e) => {
     e.target.parentNode.classList.add("active");
   }
 });
+
+navbar.addEventListener("click", (e) => {
+  if (e.target.className == "hamburger-link") {
+    hamburgerLinks.style.display = "none";
+    document.querySelector(".active").classList.remove("active");
+  }
+});
+
+// hamburger menu
+function handleHamburgerMenu() {
+  if (hamburgerLinks.style.display === "block") {
+    hamburgerLinks.style.display = "none";
+  } else {
+    hamburgerLinks.style.display = "block";
+  }
+}
